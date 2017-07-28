a9aefe2edd06859ed1d01f4e5bad29cc523837b8
Eduard <esimonyan2014@gmail.com>
2017-07-28 22:47:46 +0400

    Mongo change

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@
 module.exports = {
     port: parseInt(process.env.PORT) || 3000,
     mode: 'development',
-    mongoURI : "mongodb://admin:admin@ds151202.mlab.com:51202/heroku_vc9thf6q",
+    mongoURI : "mongodb://admin:admin@ds127163.mlab.com:27163/heroku_3l4g3mtk",
     jwtSecret: "ewvew327fv439@77fveh00@#cvegc",
 };
